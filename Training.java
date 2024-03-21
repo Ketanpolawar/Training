7acad79912a4849b94ba501d0a3ac65eac035b12
Ketan Polawar <ketanpolawar@gmail.com>
2024-03-21 10:22:12 +0530

    Linked LIst

--- Training.java
+++ Training.java
@@ -23,3 +23,53 @@ class Main {
         
     }
 }
+
+
+//Linked List
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class LinkedList{
+    public static void main(String[] args){
+        
+        LinkedList obj = new LinkedList();
+        obj.addLast(1);
+        obj.addLast(2);
+        obj.addLast(3);
+        obj.addLast(4);
+        obj.display();
+    }
+    
+
+class Node{
+    Node(int val){
+        this.data=val;
+    }
+    int data;
+    Node next;
+}
+Node head;
+
+
+public void addLast(int value){
+    Node temp = head;
+    Node obj =new Node(value);
+    if(temp==null){
+        head = obj;
+        return;
+    }
+    while(temp.next != null){
+        temp=temp.next;
+    }
+    temp.next=obj;
+}
+
+public void display(){
+    Node temp =head;
+    while(temp!=null){
+        System.out.print(temp.data+"=>");
+        temp=temp.next;
+    }
+}
+}
